797c1a2ff0fe056607a98cb9f5eee641a4c0735c
DhananjayRaut <asmita.belhekar@gmail.com>
2020-05-12 10:43:26 +0530

    category added

--- src/app/components/category/categorylist/categorylist.component.ts
+++ src/app/components/category/categorylist/categorylist.component.ts
@@ -13,59 +13,82 @@ import { PopupComponent } from '../../showpopup/popup/popup.component';
 export class CategorylistComponent implements OnInit {
 
 
-  config : any;
+  config: any;
   totalCount = 0;
   columnArray: any = [
     // { "name": "No", "key": "id" },
     { "name": "Category Name", "key": "name" },
     { "name": "Image", "key": "image" }
   ];
-  
+
   currentPage = 0;
-  dataArray : any = [];
- 
-  pageIndex : any = 0;
-  lastPage : any = 0;
+  dataArray: any = [];
 
-  url = environment.main_url + "category/" + 0 + "/sub-category" ;
+  pageIndex: any = 0;
+  lastPage: any = 0;
+
+  url = environment.main_url + "category/" + 0 + "/sub-category";
 
 
-  constructor(public router : Router,
-    public dialog : MatDialog,
-    public apiCall : ApiService) { }
+  constructor(public router: Router,
+    public dialog: MatDialog,
+    public apiCall: ApiService) { }
 
   ngOnInit() {
     this.getCategoryList();
   }
 
-  getCategoryList(){
+  getCategoryList() {
 
-    this.apiCall.get(this.url).subscribe((response)=>{
+    this.apiCall.get(this.url).subscribe((response) => {
 
       this.dataArray = response['result']['list'];
       this.totalCount = response['result']['count'];
       this.lastPage = Math.ceil(this.totalCount / 10);
     })
   }
-ngOnChanges(){
-  this.getCategoryList();
-}
-  
-  add(event){
-this.showPopup();
+  ngOnChanges() {
+    this.getCategoryList();
+  }
+
+  add(event) {
+    this.showPopup();
     // console.log("delete event",event);
     // this.router.navigate(['admin/addcategory']);
   }
 
   showPopup() {
+    let send_data = {};
+    send_data['status'] = "add";
     const dialogRef = this.dialog.open(PopupComponent, {
       width: '40%',
-      panelClass : 'custom-dialog-container'
-      // data: send_data
+      panelClass: 'custom-dialog-container',
+      data: send_data
     });
 
     dialogRef.afterClosed().subscribe(async result => {
       this.getCategoryList();
     });
+  }
+
+  edit(event){
+    console.log("show edit event:"+JSON.stringify(event));
+
+    let send_data = {};
+    send_data['status'] = "update";
+    send_data['id'] = event.id;
+    send_data['name'] = event.name;
+    send_data['image'] = event.image;
+    
+    const dialogRef = this.dialog.open(PopupComponent, {
+      width: '35%',
+      panelClass: 'custom-dialog-container',
+      data: send_data
+    });
+
+    dialogRef.afterClosed().subscribe(async result => {
+      this.getCategoryList();
+    });
+   
   }
 }
--- src/app/components/showpopup/popup/popup.component.ts
+++ src/app/components/showpopup/popup/popup.component.ts
@@ -4,6 +4,7 @@ import { CategorylistComponent } from '../../category/categorylist/categorylist.
 import { ApiService } from 'src/app/services/api/api.service';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-popup',
@@ -16,6 +17,14 @@ export class PopupComponent implements OnInit {
   fileToUpload: any;
   urls: any = [];
   firstImage: any = "";
+  popup: any = {};
+  buttonText = "ADD";
+  button = "Add";
+
+  errorMessage: any;
+  formControl = {
+    categoryName: new FormControl('', [Validators.required]),
+  };
 
   constructor(
     public dialogRef: MatDialogRef<CategorylistComponent>,
@@ -23,13 +32,40 @@ export class PopupComponent implements OnInit {
     public changeDetectorRef: ChangeDetectorRef,
     public snackbar: MatSnackBar,
     public apiCall: ApiService,
-    public router : Router
+    public router: Router
   ) { }
 
   ngOnInit() {
+    console.log("show status of category:" + this.data.id);
+    if (this.data.status == "add") {
+      this.buttonText = "ADD";
+      this.button = "Add";
+    } else {
+      this.buttonText = "UPDATE";
+      this.button = "Update";
+      this.popup['categoryName'] = this.data.name;
+      this.firstImage = this.data.image;
+    }
   }
 
+  updateCategory(categoryId) {
+    if (this.firstImage == "") {
+      this.openSnackBar("Please upload category image");
+    } else {
+    let send_date = {};
+    send_date['name'] = this.popup['categoryName'];
+    send_date['image'] = this.firstImage;
+    send_date['isActive'] = 1;
+    send_date['sequenceNumber'] = 0;
 
+    let url = environment.main_url + "category/" + 0 + "/sub-category/" + categoryId;
+    this.apiCall.put(url, send_date).subscribe(MyResponse => {
+      this.openSnackBar("Category updated successfully.")
+      this.dialogRef.close();
+    }, error => {
+    });
+  }
+  }
 
   detectEventGallery(event) {
     console.log(event);
@@ -72,23 +108,38 @@ export class PopupComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  addCategory() {
+    if (this.firstImage == "") {
+      this.openSnackBar("Please upload category image");
+    } else {
+      let send_data = {};
+
+      send_data['name'] = this.categoryName;
+      send_data['image'] = this.firstImage;
+      send_data['isActive'] = 1;
+      send_data['sequenceNumber'] = 0;
+
+      let url = environment.main_url + "category/" + 0 + "/sub-category";
+      this.apiCall.post(url, send_data).subscribe(MyResponse => {
+
+        this.dialogRef.close();
+
+      }, error => {
+
+        this.openSnackBar("Something went wrong.");
+      });
+    }
+
+  }
+
   submit() {
-    let send_data = {};
+    if (this.buttonText == "ADD") {
+      this.addCategory();
+    } else {
+      this.updateCategory(this.data.id);
 
-    send_data['name'] = this.categoryName;
-    send_data['image'] = this.firstImage;
-    send_data['isActive'] = 1;
-    send_data['sequenceNumber'] = 0;
+    }
 
-    let url = environment.main_url + "category/" + 0 + "/sub-category";
-    this.apiCall.post(url, send_data).subscribe(MyResponse => {
-      
-      this.dialogRef.close();
-
-    }, error => {
-
-      this.openSnackBar("Something went wrong.");
-    });
 
 
   }
@@ -99,5 +150,9 @@ export class PopupComponent implements OnInit {
       horizontalPosition: 'right',
       verticalPosition: 'top',
     });
+  }
+
+  removeImage(){
+    this.firstImage = "";
   }
 }
